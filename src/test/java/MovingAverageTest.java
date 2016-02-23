c0a907d6c5b76b8e5bb0b31b19ab5597d9f78339
navdeep-G <mr.navdeepgill@gmail.com>
2016-02-23 11:45:55 -0800

    Add test for simple moving average

---
+++ src/test/java/MovingAverageTest.java
@@ -0,0 +1,29 @@
+package test.java;
+import main.java.collect.MovingAverage;
+import main.java.collect.TSCollect;
+import java.util.ArrayList;
+
+import java.io.IOException;
+
+
+/**Simple way to check output of MovingAverage.
+ *
+ * @author navdeepgill
+ */
+public class MovingAverageTest {
+    public static int lag = 2;
+    public static  String pathToData = "data/birth.txt";
+
+    public static void main(String[] args) throws IOException {
+        TSCollect _tm = new TSCollect(pathToData,lag,lag);
+        ArrayList<Double> testData = _tm.ReadFile();
+        int[] windowSizes = {2};
+        for (int windSize : windowSizes) {
+            MovingAverage ma = new MovingAverage(windSize);
+            for (double x : testData) {
+                ma.newNum(x);
+                System.out.println("Next number = " + x + ", SMA = " + ma.getAvg());
+            }
+        }
+    }
+}
