40ce10a889ac729863cdf7548d2592eb1ba0248c
navdeep-G <mr.navdeepgill@gmail.com>
2016-03-07 13:58:32 -0800

    Change from interface to class implementation

--- src/main/java/util/transform/TSTransform.java
+++ src/main/java/util/transform/TSTransform.java
@@ -1,5 +1,6 @@
 package main.java.util.transform;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.ArrayList;
 import java.lang.Math;
@@ -15,63 +16,51 @@ public class TSTransform {
     public enum Type {
         LOG, ROOT_2, ROOT_3,ROOT_4;
     }
-
-    static TSTransformFunction getFunction(Type t) {
+    public static List<Double> getTransform(List<Double> data,Type t) throws IOException {
         switch(t) {
             case LOG:
-                return new Log();
+                return Log(data);
             case ROOT_2:
-                return new Root_2();
+                return Root_2(data);
             case ROOT_3:
-                return new Root_3();
+                return Root_3(data);
             case ROOT_4:
-                return new Root_4();
+                return Root_4(data);
             default:
                 throw new IllegalArgumentException("Transform function of type '" + t.name() + "' doesn't exist");
         }
     }
-}
 
-interface TSTransformFunction {
-    List<Double> exec(List<Double> l);
-}
-
-class Log implements TSTransformFunction {
-    public List<Double> exec (List<Double> l) {
-        List<Double> t_list = new ArrayList<Double>();
-        for (double i : l) {
+    public static List<Double> Log(List<Double> data){
+        List<Double> t_list = new ArrayList<>();
+        for (double i : data) {
             t_list.add(Math.log(i));
         }
         return t_list;
     }
-}
 
-class Root_2 implements TSTransformFunction {
-    public List<Double> exec (List<Double> l) {
-        List<Double> t_list = new ArrayList<Double>();
-        for (double i : l) {
+    public static List<Double> Root_2(List<Double> data){
+        List<Double> t_list = new ArrayList<>();
+        for (double i : data) {
             t_list.add(Math.sqrt(i));
         }
         return t_list;
     }
-}
 
-class Root_3 implements TSTransformFunction {
-    public List<Double> exec (List<Double> l) {
-        List<Double> t_list = new ArrayList<Double>();
-        for (double i : l) {
+    public static List<Double> Root_3(List<Double> data){
+        List<Double> t_list = new ArrayList<>();
+        for (double i : data) {
             t_list.add(Math.cbrt(i));
         }
         return t_list;
     }
-}
 
-class Root_4 implements TSTransformFunction{
-    public List<Double> exec (List<Double> l) {
-        List<Double> t_list = new ArrayList<Double>();
-        for(double i : l){
+    public static List<Double> Root_4(List<Double> data){
+        List<Double> t_list = new ArrayList<>();
+        for (double i : data) {
             t_list.add(Math.pow(i,1.0/4));
         }
         return t_list;
     }
+
 }
