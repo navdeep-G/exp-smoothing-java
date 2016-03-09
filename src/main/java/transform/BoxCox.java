597eeb97a65bfc6a4660e64f0cb2fce1314a1dac
navdeep-G <mr.navdeepgill@gmail.com>
2016-03-09 10:45:46 -0800

    Add comments and explanations

--- src/main/java/transform/BoxCox.java
+++ src/main/java/transform/BoxCox.java
@@ -17,13 +17,25 @@ import java.util.List;
  */
 public class BoxCox {
 
+
     /**
-     * Calculate a Box Cox Transformation for a given lambda
+     *Find the optimal lambda for a given time series data set and conduct transformation
      *
      *@param  data a List<Double> of time series data
-     * @param lam desired lambda for transformation
      *
-     * @return  Time series List<Double> with desired Box Cox transformation
+     *@return  Time series List<Double> with optimal Box Cox lambda transformation
+     */
+    public static List<Double> transform(List<Double> data) {
+        return transform(data, lambdaSearch(data));
+    }
+
+    /**
+     *Calculate a Box Cox Transformation for a given lambda
+     *
+     *@param  data a List<Double> of time series data
+     *@param lam desired lambda for transformation
+     *
+     *@return  Time series List<Double> with desired Box Cox transformation
      */
      public static List<Double> transform(List<Double> data, double lam) {
         List<Double> transform = new ArrayList<Double>();
@@ -41,25 +53,45 @@ public class BoxCox {
         return transform;
     }
 
-    public static List<Double> transform(List<Double> data) {
-        return BoxCox.transform(data, guerrero(data));
+    /**
+     *Find the optimal lambda for a given time series data set with default lower/upper bounds for lambda search
+     *
+     *@param  data a List<Double> of time series data
+     *
+     *@return  Time series List<Double> with optimal Box Cox lambda transformation
+     */
+    public static double lambdaSearch(final List<Double> data) {
+        return lambdaSearch(data, -1, 2);
     }
 
-    public static double guerrero(final List<Double> data) {
-        return guerrero(data, -1, 2);
-    }
-
-    public static double guerrero(final List<Double> data, double lower, double upper){
+    /**
+     *Find the optimal lambda for a given time series data set given lower/upper bounds for lambda search
+     *
+     *@param  data a List<Double> of time series data
+     *@param lower lower bound for lambda search
+     *@param upper upper bound for lambda search
+     *
+     * @return  Time series List<Double> with optimal Box Cox lambda transformation
+     */
+    public static double lambdaSearch(final List<Double> data, double lower, double upper){
         UnivariateOptimizer solver = new BrentOptimizer(1e-10, 1e-14);
         double lambda = solver.optimize(100,new UnivariateFunction(){
             public double value(double x){
-                return guer_cv(data,x);
+                return lambdaCV(data, x);
             }
         }, GoalType.MINIMIZE,lower,upper).getPoint();
         return lambda;
     }
 
-    private static double guer_cv(List<Double> data, double lam){
+    /**
+     * Compute the coefficient of variation
+     *
+     * @param data a List<Double> of time series data
+     * @param lam lambda
+     *
+     * @return Coefficient of Variation
+     */
+    private static double lambdaCV(List<Double> data, double lam){
         Iterator<Double> iter = data.iterator();
         List<Double> avg = new ArrayList<Double>();
         List<Double> result = new ArrayList<Double>();
