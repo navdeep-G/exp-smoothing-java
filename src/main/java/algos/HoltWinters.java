ea7ee381711e99e829f38ffcb119b45eb465ca6e
navdeep-G <mr.navdeepgill@gmail.com>
2016-03-17 15:14:19 -0700

    WIP: Refactor Holt Winters

--- src/main/java/algos/HoltWinters.java
+++ src/main/java/algos/HoltWinters.java
@@ -1,78 +1,264 @@
 package algos;
+import java.util.Arrays;
 import java.util.List;
-
+import java.util.ArrayList;
+/**
+ * Given a time series, say a complete monthly data for 12 months, the
+ * Holt-Winters smoothing and forecasting technique is built on the following
+ * formulae (multiplicative version):
+ *
+ * St[i] = alpha * y[i] / It[i - period] + (1.0 - alpha) * (St[i - 1] + Bt[i - 1])
+ * Bt[i] = gamma * (St[i] - St[i - 1]) + (1 - gamma) * Bt[i - 1]
+ * It[i] = beta * y[i] / St[i] + (1.0 - beta) * It[i - period]
+ * Ft[i + m] = (St[i] + (m * Bt[i])) * It[i - period + m]
+ *
+ * Note: Many authors suggest calculating initial values of St, Bt and It in a
+ * variety of ways, but some of them are incorrect e.g. determination of It
+ * parameter using regression. This implementation uses the NIST recommended methods.
+ *
+ * For more details, see: http://adorio-research.org/wordpress/?p=1230
+ * http://www.itl.nist.gov/div898/handbook/pmc/section4/pmc435.htm
+ *
+ * @author navdeepgill
+ *
+ */
 public class HoltWinters {
 
     /**
-     * Smooth supplied timeline internalData 3 ways - overall, by trend and by season.
+     * This method is the entry point. It calculates the initial values and
+     * returns the forecast for the future m periods.
      *
-     * @param data - list of internalData
-     * @param season_length - the number of entries that represent a 'season'. example = 7
-     * @param alpha - internalData smoothing factor. example = 0.2
-     * @param beta - trend smoothing factor. example = 0.01
-     * @param gamma - seasonality smoothing factor. example = 0.01
-     * @param dev_gamma - smoothing factor for deviations. example = 0.1
-     * @return
+     * @param y - Time series data.
+     * @param alpha - Exponential smoothing coefficients for level, trend,
+     *            seasonal components.
+     * @param beta - Exponential smoothing coefficients for level, trend,
+     *            seasonal components.
+     * @param gamma - Exponential smoothing coefficients for level, trend,
+     *            seasonal components.
+     * @param period - A complete season's data consists of L periods. And we need
+     *            to estimate the trend factor from one period to the next. To
+     *            accomplish this, it is advisable to use two complete seasons;
+     *            that is, 2L periods.
+     * @param m - Extrapolated future data points.
+     *          - 4 quarterly,
+     *          - 7 weekly,
+     *          - 12 monthly
+     *
+     * @param debug - Print debug values. Useful for testing.
+     *
      */
-    public static double holtWintersSmoothing(List<Double> data, int season_length, double alpha, double beta, double gamma, double dev_gamma) {
-        int n = data.size();
-        // Calculate an initial trend level
-        double trend1 = 0.0;
-        for(int i = 0; i < season_length; ++i) {
-            trend1 += data.get(i);
-        }
-        trend1 /= season_length;
+    public static List<Double> forecast(List<Double> y, double alpha, double beta,
+                                    double gamma, int period, int m, boolean debug) {
 
-        double trend2 = 0.0;
-        for(int i = season_length; i < 2*season_length; ++i) {
-            trend2 += data.get(i);
-        }
-        trend2 /= season_length;
+        validateArguments(y, alpha, beta, gamma, period, m);
 
-        double initial_trend = (trend2 - trend1) / season_length;
+        int seasons = y.size() / period;
+        double a0 = calculateInitialLevel(y);
+        double b0 = calculateInitialTrend(y, period);
+        List<Double> initialSeasonalIndices = calculateSeasonalIndices(y, period,
+                seasons);
 
-        // Take the first value as the initial level
-        double initial_level = data.get(0);
-
-        // Build index
-        double[] index = new double[n];
-        for(int i = 0; i<n; ++i) {
-            double val = data.get(i);
-            index[i] = val / (initial_level + (i + 1.0) * initial_trend);
+        if (debug) {
+            System.out.println(String.format(
+                    "Total observations: %d, Seasons %d, Periods %d", y.size(),
+                    seasons, period));
+            System.out.println("Initial level value a0: " + a0);
+            System.out.println("Initial trend value b0: " + b0);
+            printArray("Seasonal Indices: ", initialSeasonalIndices);
         }
 
-        // Build season buffer
-        double[] season = new double[n+season_length];
-        double sum=0.0;
-        for(int i = 0; i < season_length; ++i) {
-            season[i] = (index[i] + index[i+season_length]) / 2;
-            sum+=season[i];
-        }
-        index=null;
+        List<Double> forecast = calculateHoltWinters(y, a0, b0, alpha, beta, gamma,
+                initialSeasonalIndices, period, m, debug);
 
-        // Normalise season
-        double season_factor = season_length / sum;
-        for(int i = 0; i < season_length; ++i) {
-            season[i] *= season_factor;
+        if (debug) {
+            printArray("Forecast", forecast);
         }
 
-        double alpha_level = initial_level;
-        double beta_trend = initial_trend;
-        int i = 0;
-        for(; i < n; ++i) {
-            double value = data.get(i);
-            double temp_level = alpha_level;
-            double temp_trend = beta_trend;
-
-            alpha_level = alpha * value / season[i] + (1.0 - alpha) * (temp_level + temp_trend);
-            beta_trend = beta * (alpha_level - temp_level) + ( 1.0 - beta ) * temp_trend;
-
-            season[i + season_length] = gamma * value / alpha_level + (1.0 - gamma) * season[i];
-        }
-
-        double holtWinterTplus1=alpha_level + beta_trend * season[i + 1];
-
-        return holtWinterTplus1;
+        return forecast;
     }
 
+    public static List<Double> forecast(List<Double> y, double alpha, double beta,
+                                    double gamma, int period, int m) {
+        return forecast(y, alpha, beta, gamma, period, m, false);
+    }
+
+    /**
+     * Validate input.
+     *
+     * @param y
+     * @param alpha
+     * @param beta
+     * @param gamma
+     * @param m
+     */
+    private static void validateArguments(List<Double> y, double alpha, double beta,
+                                          double gamma, int period, int m) {
+        if (y == null) {
+            throw new IllegalArgumentException("Value of y should be not null");
+        }
+
+        if(m <= 0){
+            throw new IllegalArgumentException("Value of m must be greater than 0.");
+        }
+
+        if(m > period){
+            throw new IllegalArgumentException("Value of m must be <= period.");
+        }
+
+        if((alpha < 0.0) || (alpha > 1.0)){
+            throw new IllegalArgumentException("Value of Alpha should satisfy 0.0 <= alpha <= 1.0");
+        }
+
+        if((beta < 0.0) || (beta > 1.0)){
+            throw new IllegalArgumentException("Value of Beta should satisfy 0.0 <= beta <= 1.0");
+        }
+
+        if((gamma < 0.0) || (gamma > 1.0)){
+            throw new IllegalArgumentException("Value of Gamma should satisfy 0.0 <= gamma <= 1.0");
+        }
+    }
+
+    /**
+     * This method realizes the Holt-Winters equations.
+     *
+     * @param y
+     * @param a0
+     * @param b0
+     * @param alpha
+     * @param beta
+     * @param gamma
+     * @param initialSeasonalIndices
+     * @param period
+     * @param m
+     * @param debug
+     * @return - Forecast for m periods.
+     */
+    private static List<Double> calculateHoltWinters(List<Double> y, double a0, double b0,
+                                                 double alpha, double beta, double gamma,
+                                                 List<Double> initialSeasonalIndices, int period, int m, boolean debug) {
+
+        List<Double> St = new ArrayList<Double>(y.size());
+        List<Double> Bt = new ArrayList<Double>(y.size());
+        List<Double> It = new ArrayList<Double>(y.size());
+        List<Double> Ft = new ArrayList<Double>(y.size() + m);
+
+        // Initialize base values
+        St.add(1, a0);
+        Bt.add(1, b0);
+
+        for (int i = 0; i < period; i++) {
+            It.add(i,initialSeasonalIndices.get(i));
+        }
+
+        // Start calculations
+        for (int i = 2; i < y.size(); i++) {
+
+            // Calculate overall smoothing
+            if ((i - period) >= 0) {
+                St.add(i, alpha * y.get(i) / It.get(i - period) + (1.0 - alpha)
+                        * (St.get(i - 1) + Bt.get(i - 1)));
+            } else {
+                St.add(i, alpha * y.get(i) + (1.0 - alpha) * (St.get(i - 1) + Bt.get(i - 1)));
+            }
+
+            // Calculate trend smoothing
+            Bt.add(i, gamma * (St.get(i) - St.get(i - 1) + (1 - gamma) * Bt.get(i - 1)));
+
+            // Calculate seasonal smoothing
+            if ((i - period) >= 0) {
+                It.add(i, beta * y.get(i) / St.get(i) + (1.0 - beta) * It.get(i - period));
+            }
+
+            // Calculate forecast
+            if (((i + m) >= period)) {
+                Ft.add(i + m, (St.get(i) + (m * Bt.get(i)) * It.get(i - period + m)));
+            }
+
+            if (debug) {
+                System.out.println(String.format(
+                        "i = %d, y = %d, S = %f, Bt = %f, It = %f, F = %f", i,
+                        y.get(i), St.get(i), Bt.get(i), It.get(i), Ft.get(i)));
+            }
+        }
+
+        return Ft;
+    }
+
+    /**
+     * See: http://robjhyndman.com/researchtips/hw-initialization/ 1st period's
+     * average can be taken. But y[0] works better.
+     *
+     * @return - Initial Level value i.e. St[1]
+     */
+    private static double calculateInitialLevel(List<Double> y) {
+        return y.get(0);
+    }
+
+    /**
+     * See: http://www.itl.nist.gov/div898/handbook/pmc/section4/pmc435.htm
+     *
+     * @return - Initial trend - Bt[1]
+     */
+    private static double calculateInitialTrend(List<Double> y, int period) {
+
+        double sum = 0;
+
+        for (int i = 0; i < period; i++) {
+            sum += (y.get(period + i) - y.get(i));
+        }
+
+        return sum / (period * period);
+    }
+
+    /**
+     * See: http://www.itl.nist.gov/div898/handbook/pmc/section4/pmc435.htm
+     *
+     * @return - Seasonal Indices.
+     */
+    private static List<Double> calculateSeasonalIndices(List<Double> y, int period,
+                                                     int seasons) {
+
+        double[] seasonalAverage = new double[seasons];
+        double[] seasonalIndices = new double[period];
+
+        double[] averagedObservations = new double[y.size()];
+
+        for (int i = 0; i < seasons; i++) {
+            for (int j = 0; j < period; j++) {
+                seasonalAverage[i] += y.get((i * period) + j);
+            }
+            seasonalAverage[i] /= period;
+        }
+
+        for (int i = 0; i < seasons; i++) {
+            for (int j = 0; j < period; j++) {
+                averagedObservations[(i * period) + j] = y.get((i * period) + j)
+                        / seasonalAverage[i];
+            }
+        }
+
+        for (int i = 0; i < period; i++) {
+            for (int j = 0; j < seasons; j++) {
+                seasonalIndices[i] += averagedObservations[(j * period) + i];
+            }
+            seasonalIndices[i] /= seasons;
+        }
+
+        ArrayList<Double> list = new ArrayList<Double>(seasonalIndices.length);
+        for(double d : seasonalIndices) list.add(d);
+        return list;
+    }
+
+    /**
+     * Utility method to print array values.
+     *
+     * @param description
+     * @param data
+     */
+    private static void printArray(String description, List<Double> data) {
+        System.out.println(description);
+        for (int i = 0; i < data.size(); i++) {
+            System.out.println(data.get(i));
+        }
+    }
 }
