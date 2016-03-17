2b061c8729d38ac45b6da44dc59c8f5d0d4318b8
navdeep-G <mr.navdeepgill@gmail.com>
2016-03-17 12:04:02 -0700

    Return List<Double> after SMA calculation

--- src/main/java/transform/SimpleMovingAverage.java
+++ src/main/java/transform/SimpleMovingAverage.java
@@ -1,21 +1,31 @@
 package transform;
 
+import collect.Collect;
+
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.lang.*;
-
+import java.util.List;
 
 /**Simple Moving Average (SMA)
  *
  * @author navdeepgill
  */
 
-//TODO Return List<Double>
-
 public class SimpleMovingAverage {
     Queue<Double> window = new LinkedList<Double>();
     private final int period;
     private double sum;
+
+    public List<Double> getMA(List<Double> data){
+        List<Double> ma_data = new ArrayList<Double>(data.size());
+            for (double x : data) {
+                newNum(x);
+                ma_data.add(getAvg());
+        }
+        return ma_data;
+    }
 
     public SimpleMovingAverage(int period) {
         assert period > 0 : "Period must be a positive integer!";
@@ -34,7 +44,6 @@ public class SimpleMovingAverage {
         if (window.isEmpty()) return 0; // technically the average is undefined
         return sum / window.size();
     }
-
 
 }
 
