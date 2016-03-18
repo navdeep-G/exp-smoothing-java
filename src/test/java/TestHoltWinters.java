14b86c7b127101d3e55adfad76fddb41f9af2295
navdeep-G <mr.navdeepgill@gmail.com>
2016-03-17 19:57:51 -0700

    Update test

--- src/test/java/TestHoltWinters.java
+++ src/test/java/TestHoltWinters.java
@@ -1,5 +1,8 @@
 import org.junit.Test;
 import algos.HoltWinters;
+import util.Util;
+
+import java.io.IOException;
 import java.util.List;
 import java.util.Arrays;
 import static org.junit.Assert.*;
@@ -7,35 +10,21 @@ import static org.junit.Assert.*;
 public class TestHoltWinters {
 
     @Test
-    public void forecastNISTData() {
-
-        /**
-         * NIST data available at:
-         * http://www.itl.nist.gov/div898/handbook/pmc/section4/pmc436.htm
-         */
-        List<Double> y = Arrays.asList(362.0, 385.0, 432.0, 341.0, 382.0, 409.0, 498.0, 387.0, 473.0, 513.0, 582.0, 474.0,
-                544.0, 582.0, 681.0, 557.0, 628.0, 707.0, 773.0, 592.0, 627.0, 725.0, 854.0, 661.0);
-        int period = 4;
-        int m = 4;
-
+    public void forecastNISTData() throws IOException {
+        String pathToData = "data/hotel.txt";
+        List<Double> file = Util.ReadFile(pathToData);
+        int period = 12;
+        int m = 12;
+        boolean debug = true;
         double alpha = 0.5;
         double beta = 0.4;
         double gamma = 0.6;
 
-        List<Double> prediction = HoltWinters.forecast(y, alpha, beta, gamma,
-                period, m);
+        List<Double> prediction = HoltWinters.forecast(file, alpha, beta, gamma,
+                period, m, debug);
 
         // These are the expected results
-        List<Double> expected = Arrays.asList(0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-                594.8043646513713, 357.12171044215734, 410.9203094983815,
-                444.67743912921156, 550.9296957593741, 421.1681718160631,
-                565.905732450577, 639.2910221068818, 688.8541669002238,
-                532.7122406111591, 620.5492369959037, 668.5662327429854,
-                773.5946568453546, 629.0602103529998, 717.0290609530134,
-                836.4643466657625, 884.1797655866865, 617.6686414831381,
-                599.1184450128665, 733.227872348479, 949.0708357438998,
-                748.6618488792186);
-        assertEquals(String.valueOf(expected),
-                String.valueOf(prediction));
+        List<Double> expected = Arrays.asList(819.5);
+        assertEquals(expected, prediction);
     }
 }
