7ad05eb1d56a5aa1447b3181e1f8b39502c850c8
navdeep-G <mr.navdeepgill@gmail.com>
2016-03-18 17:47:31 -0700

    Add method to get cumulative moving average

--- src/main/java/movingaverage/CumulativeMovingAverage.java
+++ src/main/java/movingaverage/CumulativeMovingAverage.java
@@ -1,5 +1,8 @@
 package movingaverage;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**Cumulative Moving Average (CMA)
  *
  * @author navdeepgill
@@ -8,6 +11,15 @@ package movingaverage;
 public class CumulativeMovingAverage {
     int n = 0;
     double average = 0.0;
+
+    public List<Double> getCMA(List<Double> data){
+        List<Double> cma_data = new ArrayList<Double>(data.size());
+
+        for (int i=0;i<data.size();i++) {
+            cma_data.add(add(data.get(i)));
+        }
+        return cma_data;
+    }
 
     public double add(double x) {
         return average += (x - average) / ++n;
