24ed424c8c9e625e4f57379671d4fde077fec01e
navdeep-G <mr.navdeepgill@gmail.com>
2016-03-22 15:07:03 -0700

    Update StatsFrame and Frame Tests

--- src/main/java/util/StatsFrame.java
+++ src/main/java/util/StatsFrame.java
@@ -3,111 +3,129 @@ package util;
 import java.util.Collections;
 import java.util.List;
 import water.fvec.Frame;
+import water.fvec.Vec;
 
 /**Collect relevant statistics about a time series
+ *
  * @author navdeepgill
  */
 public class StatsFrame {
+
     public static double[] average(Frame data) {
         return data.means();
     }
-}
-   /** public static double variance(List<Double> data) {
-        double avg = average(data);
-        double total = 0;
-        double nrow = data.size();
-        if (nrow == 1) return 0.0;
 
-        for (double item : data) {
-            total += (item - avg) * (item - avg);
+    public static double[] standardDeviation(Frame data) {
+        double[] vec = new double[data.numCols()];
+
+        for (int i = 0; i < vec.length; i++) {
+            vec[i] = data.vec(i).sigma();
         }
-
-        double variance = total/(nrow-1);
-        return variance;
+        return vec;
     }
 
-    public static double standardDeviation(List<Double> data) {
-        return Math.sqrt(variance(data));
+    public static double[] variance(Frame data) {
+        double[] vec = new double[data.numCols()];
+
+        for (int i = 0; i < vec.length; i++) {
+            vec[i] = Math.pow(data.vec(i).sigma(),2);
+        }
+        return vec;
     }
 
-    public static int getMinimumIndex(List<Double> data){
-        return data.indexOf(Collections.min(data));
+    public static double[] getMin(Frame data) {
+        double[] vec = new double[data.numCols()];
+
+        for (int i = 0; i < vec.length; i++) {
+            vec[i] = data.vec(i).min();
+        }
+        return vec;
     }
 
-    public static int getMaximumIndex(List<Double> data){
-        return data.indexOf(Collections.max(data));
+    public static double[] getMax(Frame data) {
+        double[] vec = new double[data.numCols()];
+
+        for (int i = 0; i < vec.length; i++) {
+            vec[i] = data.vec(i).max();
+        }
+        return vec;
     }
 
-    public static double getMinimum(List<Double> data){
-        int MinIndex = getMinimumIndex(data);
-        return data.get(MinIndex);
-    }
-
-    public static double getMaximium(List<Double> data){
-        int MaxIndex = getMaximumIndex(data);
-        return data.get(MaxIndex);
-    }
-
-    public static double getAutoCovariance(List<Double> data, int k){
-        double nrow = data.size();
+    public static double [] getAutoCovariance(Frame data, int k){
+        long nrow = data.numRows();
 
         if (k == 0) {
             return variance(data);
         }
 
-        double total = 0;
-        double avg = average(data);
-        for (int i = k; i < data.size(); i++) {
-            total += (data.get(i - k) - avg) * (data.get(i) - avg);
+        double[] auto_cov = new double[data.numCols()];
+        double[] avg = average(data);
+
+        for(int v = 0; v < data.numCols(); ++v){
+            Vec vec = data.vec(v);
+                for (long i = k; i < nrow; ++i) {
+                    auto_cov[v] += (vec.at(i - k) - avg[v]) * (vec.at(i) - avg[v]);
+                }
+                auto_cov[v]/=nrow;
         }
-        double autocovariance = total/nrow;
-        return autocovariance;
+
+        return auto_cov;
 
     }
 
-    public static double getAutoCorrelation(List<Double> data, int k){
-        double autocovariance = getAutoCovariance(data,k);
-        double variance = variance(data);
-        double autocorrelation = autocovariance/variance;
+    public static double[] getAutoCorrelation(Frame data, int k){
+        double autocovariance[] = getAutoCovariance(data,k);
+        double variance[] = variance(data);
+        double autocorrelation[] = new double[autocovariance.length];
+
+        for(int j = 0; j < autocorrelation.length; ++j){
+            autocorrelation[j] = autocovariance[j]/variance[j];
+        }
+
         return autocorrelation;
     }
 
-    public static double[] getAcf(List<Double> data, int n){
 
-        double[] acfValues = new double[n + 1];
+    public static double[][] getAcf(Frame data, int n){
+
+        double[][] acfValues = new double[data.numCols()][n + 1];
 
         for (int i = 0; i <= n; i++) {
-            acfValues[i] = getAutoCorrelation(data, i);
+            double[] acf = getAutoCorrelation(data,i);
+                for(int j = 0; j < data.numCols(); ++j){
+                    acfValues[j][i] = acf[j];
+                }
         }
 
         return acfValues;
     }
 
-    public static double[] getPacf(List<Double> data, int n){
+//    public static double[] getPacf(Frame data, int n){
+//
+//        double[] pacfValues = new double[n + 1];
+//        double[][] phi = new double[n + 1][n + 1];
+//
+//        pacfValues[0] = phi[0][0] = 1D;
+//        pacfValues[1] = phi[1][1] = getAutoCorrelation(data, 1);
+//
+//        for (int i = 2; i <= n; i++) {
+//            for (int j = 1; j < i - 1; j++) {
+//                phi[i - 1][j] = phi[i - 2][j] - phi[i - 1][i - 1]
+//                        * phi[i - 2][i - 1 - j];
+//            }
+//
+//            double a = 0D, b = 0D;
+//            for (int j = 1; j < i; j++) {
+//                a += phi[i - 1][j] * getAutoCorrelation(data, i - j);
+//                b += phi[i - 1][j] * getAutoCorrelation(data, j);
+//            }
+//
+//            pacfValues[i] = phi[i][i] = (getAutoCorrelation(data, i) - a)
+//                    / (1 - b);
+//        }
+//
+//        return pacfValues;
+//    }
 
-        double[] pacfValues = new double[n + 1];
-        double[][] phi = new double[n + 1][n + 1];
 
-        pacfValues[0] = phi[0][0] = 1D;
-        pacfValues[1] = phi[1][1] = getAutoCorrelation(data, 1);
-
-        for (int i = 2; i <= n; i++) {
-            for (int j = 1; j < i - 1; j++) {
-                phi[i - 1][j] = phi[i - 2][j] - phi[i - 1][i - 1]
-                        * phi[i - 2][i - 1 - j];
-            }
-
-            double a = 0D, b = 0D;
-            for (int j = 1; j < i; j++) {
-                a += phi[i - 1][j] * getAutoCorrelation(data, i - j);
-                b += phi[i - 1][j] * getAutoCorrelation(data, j);
-            }
-
-            pacfValues[i] = phi[i][i] = (getAutoCorrelation(data, i) - a)
-                    / (1 - b);
-        }
-
-        return pacfValues;
-    }
 }
-**/
--- src/test/java/TestCollectFrame.java
+++ src/test/java/TestCollectFrame.java
@@ -1,5 +1,6 @@
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import algos.*;
 import collect.Collect;
@@ -77,15 +78,6 @@ public class TestCollectFrame extends TestUtil {
          }
          System.out.println("\n");
 
-        /*
-         System.out.println("First 10 Rows of Time Series Dataset: " + pathToData);
-         for(int i = 0; i < 10; i++) {
-         System.out.println(file.get(i));
-         }
-         System.out.println("\n");
-
-         System.out.println("Number of rows for " + pathToData + " = " + file.size());
-         System.out.println("\n");*/
 
         System.out.println("Average of Time Series:");
         double average = Stats.average(file);
@@ -93,57 +85,83 @@ public class TestCollectFrame extends TestUtil {
         System.out.println("\n");
 
         System.out.println("Average of Time Series Using Frame:");
-        double averageFrame = StatsFrame.average(fr)[0];
-        System.out.println(averageFrame);
+        double[] averageFrame = StatsFrame.average(fr);
+        System.out.println(averageFrame[0]);
         System.out.println("\n");
 
-        /*System.out.println("Variance of Time Series:");
-         double var = _tm.getVariance();
-         System.out.println(var);
-         System.out.println("\n");
+        System.out.println("Variance of Time Series:");
+        double variance = Stats.variance(file);
+        System.out.println(variance);
+        System.out.println("\n");
 
-         System.out.println("Standard Deviation of Time Series:");
-         double sd = _tm.getStandardDeviation();
-         System.out.println(sd);
-         System.out.println("\n");
+        System.out.println("Variance of Time Series Using Frame:");
+        double[] varFrame = StatsFrame.variance(fr);
+        System.out.println(varFrame[0]);
+        System.out.println("\n");
 
-         System.out.println("Index of Time Series Minimum Value:");
-         double min = _tm.getMinIndex();
-         System.out.println(min);
-         System.out.println("\n");
+        System.out.println("Standard Deviation of Time Series:");
+        double sd = Stats.standardDeviation(file);
+        System.out.println(sd);
+        System.out.println("\n");
 
-         System.out.println("Minimum Value of Time Series Based on Index:");
-         double minVal = _tm.getMin();
-         System.out.println(minVal);
-         System.out.println("\n");
+        System.out.println("Standard Deviation of Time Series Using Frame:");
+        double[] sdFrame = StatsFrame.standardDeviation(fr);
+        System.out.println(sdFrame[0]);
+        System.out.println("\n");
 
-         System.out.println("Index of Time Series Maximum Value:");
-         double max = _tm.getMaxIndex();
-         System.out.println(max);
-         System.out.println("\n");
+        System.out.println("Minimum Value of Time Series:");
+        double minVal = Stats.getMinimum(file);
+        System.out.println(minVal);
+        System.out.println("\n");
 
-         System.out.println("Maximum Value of Time Series Based on Index:");
-         double maxVal = _tm.getMax();
-         System.out.println(maxVal);
-         System.out.println("\n");
+        System.out.println("Minimum Value of Time Series Using Frame:");
+        double[] minValFrame = StatsFrame.getMin(fr);
+        System.out.println(minValFrame[0]);
+        System.out.println("\n");
 
-         System.out.println("Autocovariance of Time Series:");
-         double autocovar = _tm.getAutocovariance();
-         System.out.println(autocovar);
-         System.out.println("\n");
+        System.out.println("Maximum Value of Time Series:");
+        double maxVal = Stats.getMaximium(file);
+        System.out.println(maxVal);
+        System.out.println("\n");
 
-         System.out.println("Autocorrelation of Time Series with lag " + lag + ":");
-         double autocor = _tm.getAutocorrelation();
-         System.out.println(autocor);
-         System.out.println("\n");
+        System.out.println("Maximum Value of Time Series Using Frame:");
+        double[] maxValFrame = StatsFrame.getMax(fr);
+        System.out.println(maxValFrame[0]);
+        System.out.println("\n");
 
-         System.out.println("Autocorrelation Function of Time Series with lag " + lag + ":");
-         double[] acf= _tm.acf(2);
-         for(int i = 0; i < acf.length; i++) {
-         System.out.println(acf[i]);
-         }
-         System.out.println("\n");
+        System.out.println("Autocovariance of Time Series:");
+        double autocovar = Stats.getAutoCovariance(file, 1);
+        System.out.println(autocovar);
+        System.out.println("\n");
 
+        System.out.println("Autocovariance of Time Series Using Frame:");
+        double[] autocovarFrame = StatsFrame.getAutoCovariance(fr,1);
+        System.out.println(autocovarFrame[0]);
+        System.out.println("\n");
+
+        System.out.println("Autocorrelation of Time Series with lag " + lag + ":");
+        double autocor = Stats.getAutoCorrelation(file, lag);
+        System.out.println(autocor);
+        System.out.println("\n");
+
+        System.out.println("Autocorrelation of Time Series Using Frame with lag " + lag + ":");
+        double[] autocorFrame = StatsFrame.getAutoCorrelation(fr,lag);
+        System.out.println(autocorFrame[0]);
+        System.out.println("\n");
+
+        System.out.println("Autocorrelation Function of Time Series with lag " + lag + ":");
+        double[] acf= Stats.getAcf(file,lag);
+        for(int i = 0; i < acf.length; i++) {
+            System.out.println(acf[i]);
+        }
+        System.out.println("\n");
+
+        System.out.println("Autocorrelation Function of Time Series Using Frame with lag " + lag + ":");
+        double[][] acfFrame= StatsFrame.getAcf(fr,lag);
+        System.out.println(Arrays.deepToString(acfFrame));
+        System.out.println("\n");
+
+         /*
          System.out.println("Partial Autocorrelation Function of Time Series with lag " +  lag + ":");
          double[] pacf= _tm.pacf();
          for(int i = 0; i < pacf.length; i++) {
