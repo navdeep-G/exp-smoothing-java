b1ceb8f2314762af9ef4f91f6705504d0564e5d2
navdeep-G <mr.navdeepgill@gmail.com>
2016-03-24 13:16:42 -0700

    No need for new list. Just modify current

--- src/main/java/movingaverage/CumulativeMovingAverage.java
+++ src/main/java/movingaverage/CumulativeMovingAverage.java
@@ -13,12 +13,10 @@ public class CumulativeMovingAverage {
     double average = 0.0;
 
     public List<Double> getCMA(List<Double> data){
-        List<Double> cma_data = new ArrayList<Double>(data.size());
-
         for (int i=0;i<data.size();i++) {
-            cma_data.add(add(data.get(i)));
+            data.set(i,add(data.get(i)));
         }
-        return cma_data;
+        return data;
     }
 
     public double add(double x) {
