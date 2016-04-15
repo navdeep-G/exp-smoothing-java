84a1f7da0e54a8654d33bbf71fbc1014280a1cf8
navdeep-G <mr.navdeepgill@gmail.com>
2016-04-15 16:07:39 -0700

    Junit for AugmentedDickeyFuller

---
+++ src/test/java/random/AugmentedDickeyFullerTest.java
@@ -0,0 +1,35 @@
+package random;
+
+import java.io.IOException;
+import java.util.Random;
+import java.util.ArrayList;
+import tests.*;
+import static org.junit.Assert.*;
+import java.util.Random;
+import org.junit.Test;
+
+public class AugmentedDickeyFullerTest {
+
+    @Test
+    public void testLinearTrend() {
+        Random rand = new Random();
+        ArrayList<Double> x = new ArrayList();
+        for (int i = 0; i < 100; i++) {
+            x.add(i, (i + 1) + 5 * rand.nextDouble());
+            }
+        AugmentedDickeyFuller adf = new AugmentedDickeyFuller(x);
+        assertTrue(adf.isNeedsDiff() == true);
+        }
+
+    @Test
+    public void testLinearTrendWithOutlier() {
+        Random rand = new Random();
+        ArrayList<Double> x = new ArrayList();
+        for (int i = 0; i < 100; i++) {
+            x.add(i,(i + 1) + 5 * rand.nextDouble());
+            }
+        x.add(50, 100.0);
+        AugmentedDickeyFuller adf = new AugmentedDickeyFuller(x);
+        assertTrue(adf.isNeedsDiff() == true);
+        }
+    }
