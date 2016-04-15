3ff2761d6a5084532a03f29c6cb18d244ead22d3
navdeep-G <mr.navdeepgill@gmail.com>
2016-04-14 18:23:24 -0700

    Update CMA to implement TransformFrame

--- src/main/java/movingaverage/CumulativeMovingAverageFrame.java
+++ src/main/java/movingaverage/CumulativeMovingAverageFrame.java
@@ -1,18 +1,22 @@
 package movingaverage;
 
+import transform.TransformFrame;
 import water.fvec.Frame;
 import water.fvec.Vec;
+
+import java.util.concurrent.ThreadLocalRandom;
 
 /**Cumulative Moving Average (CMA)
  *
  * @author navdeepgill
  */
 
-public class CumulativeMovingAverageFrame {
+public class CumulativeMovingAverageFrame implements TransformFrame {
     int n = 0;
     double average = 0.0;
 
-    public Frame getCMA(Frame data){
+    @Override
+    public Frame transform(Frame data){
         for(Vec v : data.vecs()){
             for (long x = 0; x < v.length(); ++x) {
                 v.set(x,add(v.at(x)));
