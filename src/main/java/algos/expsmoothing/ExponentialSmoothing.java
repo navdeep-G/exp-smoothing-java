a7403126d4687941e3c682ae1f154b7fb4a75c28
navdeep-G <mr.navdeepgill@gmail.com>
2025-07-06 09:13:24 -0700

    Add exponential smoothing interface

---
+++ src/main/java/algos/expsmoothing/ExponentialSmoothing.java
@@ -0,0 +1,14 @@
+package algos.expsmoothing;
+
+import java.util.List;
+
+public interface ExponentialSmoothing {
+    /**
+     * Forecasts values based on time series data.
+     *
+     * @param data the time series input
+     * @param steps number of future periods to forecast
+     * @return list of forecasted values (original data + forecast)
+     */
+    List<Double> forecast(List<Double> data, int steps);
+}
